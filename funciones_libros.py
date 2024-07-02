8e06a0f6ba9804959bf7aceadd982bf2bc2ee376
matipzzk <ma.imil@duocuc.cl>
2024-07-02 18:17:07 -0400

    Inicio

---
+++ funciones_libros.py
@@ -0,0 +1,13 @@
+import os, time
+
+    
+    
+    
+def agregar_libro():
+    os.system("cls")
+    nombre_libro = input("Ingrese nombre del libro: ")
+    autor = input("Ingrese el autor del libro: ")    
+    anios = int(input("Ingrese año de publicación del libro: "))    
+    genero= input("Ingrese el género del libro: ")    
+        
+    
---
+++ libros.py
@@ -0,0 +1,13 @@
+from  funciones_libros import *
+
+libros = []
+
+while True:
+    print("1. Agregar libro: ")
+    print("2. Mostrar  libro: ")
+    print("3. Buscar libro por titulo: ")
+    print("4. Actualizar libro: ")
+    print("5. Guardar libro en un archivo JSON: ")
+    print("6. Salir: ")
+    opc = int(input("Ingrese una opción: "))
+    
